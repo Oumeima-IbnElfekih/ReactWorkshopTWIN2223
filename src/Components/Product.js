30044d7bd576e9354411e8cffa9527ecfc178ea4
Oumeima-IbnElfekih <oumeimaibnelg@gmail.com>
2023-02-20 09:34:23 +0100

    correction atelier2.P1

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,45 +1,38 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Button, Card, Col, Row } from "react-bootstrap";
 
-class Product extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { product :this.props.product  }
-  }
+function Product(props) {
   
-  addLikes =(e) => {
-      // e.preventDefault();
-      console.log(this.state);
-      this.setState((oldState)=> ({
-          product : {...oldState.product, like : oldState.product.like +=1 }
+  const [product, setProduct] = useState(props.product)
+  const addLikes =(e) => {
+        setProduct({...product, like : product.like +=1 })
 
-      }))
   }
  
-  render() {   
+
     return (
      
       <Card
-       style={{ width: "18rem" }} className="text-center" border="secondary">
+       style={{ width: "18rem" }} className={ product.like >= 5 ? "text-center bestProduct" :"text-center" } border="secondary">
              <Card.Header>
         <Card.Img
           variant="top"
-          src={require("../assets/images/" + this.state.product.img)}
+          src={require("../assets/images/" + product.img)}
           alt="Product Img"
           height={200}
         />
         </Card.Header>
         <Card.Body>
-          <Card.Title>{this.state.product.name}</Card.Title>
-          <Card.Text>Price : {this.state.product.price} DT</Card.Text>
-          <Card.Text>Quantity :{this.state.product.quantity}</Card.Text>
-          <Card.Text>Likes :{this.state.product.like}</Card.Text>
+          <Card.Title>{product.name}</Card.Title>
+          <Card.Text>Price : {product.price} DT</Card.Text>
+          <Card.Text>Quantity :{product.quantity}</Card.Text>
+          <Card.Text>Likes :{product.like}</Card.Text>
           <Row>
             <Col>
-              <Button variant="primary" onClick={this.addLikes}>Like</Button>
+              <Button variant="primary" onClick={addLikes}>Like</Button>
             </Col>
             <Col>
-              <Button variant="dark" onClick = {()=> this.props.buyFun(this.state.product)} disabled={this.props.product.quantity===0}>Buy</Button>
+              <Button variant="dark" onClick = {()=> props.buyFun(product)} disabled={product.quantity===0}>Buy</Button>
             </Col>
             
           </Row>
@@ -47,6 +40,6 @@ class Product extends Component {
       </Card>
     );
   }
-}
+
 
 export default Product;
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,67 +1,61 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { Alert, Col, Container, Row } from "react-bootstrap";
 import products from "../products.json";
 import Product from "./Product";
 
-class Products extends Component {
-  
-  
-  constructor(props) {
-    super();
-    this.state = { showAlert :false , showMessage: true}
-  }
-  buy=(p) => {
-    if( p.quantity > 0 )
-    {p.quantity -- ;
-    this.setState({showAlert : true})
-    setTimeout(
-      ()=>  { this.setState({showAlert : false})}
-      ,2000)}
-  }
-  componentDidMount(){
-    console.log('composant a été monté ' + this.state.showMessage)
-    setTimeout(
-      ()=>  { this.setState({showMessage : false})}
-      ,5000)
+function Products(props) {
+  const [showAlert, setShowAlert] = useState(false);
+  const [showMessage, setShowMessage] = useState(true);
 
-  }
-  componentDidUpdate() {
-    console.log('composant a été modifié ' + this.state.showMessage)
-  }
-  componentWillUnmount(){
-    console.log('composant a été supprimé ' )
-  }
-  render() {
-    return (
-      <Container style={{ marginTop: "30px" }}>
-      {this.state.showMessage &&
-      <Alert variant="success">
-            <Alert.Heading>Hey, Welcome To Our Shop <strong> MyStore </strong>    </Alert.Heading>
-            <p>
-            Thank you for choosing our store, we hope you enjoy your shopping experience!
-            </p>
-            <hr />
-          </Alert> }
-      {this.state.showAlert &&
-      
-      <Alert style={{ marginTop: "30px" }} variant="primary">
-        <p>
-        You Bought an Item
-        </p>
-      
-      </Alert>
-      }
-        <Row>
-          {products.map((product, index) => (
-            <Col md={4} key={index}>
-              <Product product={product} buyFun={this.buy} />
-            </Col>
-          ))}
-        </Row>
-        
-      </Container>
-    );
-  }
+  const buy = (p) => {
+    if (p.quantity > 0) {
+      p.quantity--;
+      setShowAlert(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 2000);
+    }
+  };
+  useEffect(() => {
+    console.log("composant a été monté " + showMessage);
+    setTimeout(() => {
+      setShowMessage(false);
+    }, 5000);
+    return console.log("composant a été supprimé ");
+  }, []);
+
+  useEffect(() => {
+    console.log("composant a été modifié " + showMessage);
+  }, [showMessage]);
+
+  return (
+    <Container style={{ marginTop: "30px" }}>
+      {showMessage && (
+        <Alert variant="success">
+          <Alert.Heading>
+            Hey, Welcome To Our Shop <strong> MyStore </strong>{" "}
+          </Alert.Heading>
+          <p>
+            Thank you for choosing our store, we hope you enjoy your shopping
+            experience!
+          </p>
+          <hr />
+        </Alert>
+      )}
+      {showAlert && (
+        <Alert style={{ marginTop: "30px" }} variant="primary">
+          <p>You Bought an Item</p>
+        </Alert>
+      )}
+      <Row>
+        {products.map((product, index) => (
+          <Col md={4} key={index}>
+            <Product product={product} buyFun={buy} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
 
 export default Products;
